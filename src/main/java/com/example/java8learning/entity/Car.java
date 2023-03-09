425e2c927aaa8e8526693aa42c909a6c8743809c
“penglin” <penglin2233@163.com>
2023-03-09 23:07:00 +0800

    #comment testdevelop

--- src/main/java/com/example/java8learning/entity/Car.java
+++ src/main/java/com/example/java8learning/entity/Car.java
@@ -1,2 +1,24 @@
-package com.example.java8learning.entity;public class car {
+package com.example.java8learning.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    int id;
+    int manufacturerId;
+    String model;
+    int year;
+    float rating;
+
+    public Car(int id, int manufacturerId, String model, int year) {
+        this.id = id;
+        this.manufacturerId = manufacturerId;
+        this.model = model;
+        this.year = year;
+    }
 }
